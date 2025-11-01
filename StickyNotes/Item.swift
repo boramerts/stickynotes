c0dbb65147e4df0a3c5ee2f0d47a030eba3af1d7
boramerts <mertbora723@gmail.com>
2025-11-01 14:12:40 +0300

    Initial Commit

---
+++ StickyNotes/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  StickyNotes
+//
+//  Created by Bora Mert on 1.11.2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
